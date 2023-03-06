e4ca7c93cbf7385f976a859c0b76f7aeaf59d4a7
mohamm29 <126508171+mohamm29@users.noreply.github.com>
2023-03-06 06:20:19 +0300

    Update run.sh

--- assignment10/run.sh
+++ assignment10/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo $1 > access
-gcloud compute instances create newinstance --project=$2 --zone=us-central1-a --machine-type=e2-medium  --network-interface=network=mycustvpc,subnet=uscent --access-token-file=access
+gcloud compute instances create newinstance --project=mfa-project-374417 --zone=us-central1-a --machine-type=e2-medium  --network-interface=network=mycustvpc,subnet=uscent --access-token-file=access
