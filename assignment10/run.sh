bf38996d95c073489d05fc5fe885622209c6afd4
mohamm29 <126508171+mohamm29@users.noreply.github.com>
2023-03-06 03:34:26 +0300

    Update run.sh

--- assignment10/run.sh
+++ assignment10/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo $1 > token
-gcloud compute instances create automationvm --project=mfa-project-374417 --machine-type=e2-medium --network-interface=network=mycustvpc --zone=us-central1-a --acccess-token-file=token
+gcloud compute instances create automationvm --project=mfa-project-374417 --machine-type=e2-medium --network-interface=network=mycustvpc --zone=us-central1-a --access-token-file=token
